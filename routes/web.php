26dba483651db58297f8ef5d026e2bc3bde1df15
sekenty <serik012@gmail.com>
2022-06-13 07:26:01 +0600

    edit route
    edit route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use App\Services\CheckCharsService;
+use App\Services\MigrationDataService;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,33 +19,6 @@ Route::get('/', function () {
 });
 
 Route::get('/file', function () {
-    $status = true; //начальная ковычка
-    $result = [];
-   
-    $content = Storage::disk('public')->get('/mark_models.txt');
-    $content = str_replace( '"',"'", $content);
-    $array = preg_split('//', $content, -1, PREG_SPLIT_NO_EMPTY);
-
-    foreach ($array as $key => $value) {
-
-        if ($value == "'" && $status) {
-            $value = '"';
-            $status = false; // прошли начальную точку
-        }
-        
-        if ($value == "'" && !$status && CheckCharsService::check($array[$key],$array[$key+1], $array[$key+2]) && $array[$key+1] != '"') {
-            $value = '"';
-            $status = true; // прошли конечную точку
-        }
-
-        if ($value == '\\') {
-            $value = "";
-        }
-        array_push($result, $value);
-    }
-
-    $result = implode("", $result);
-    $result = json_decode($result, true);
-    
-    echo "<pre>"; var_dump($result); echo "<pre>";
+    $data = MigrationDataService::getData();
+    echo "<pre>"; var_dump($data); echo "<pre>";
 });
